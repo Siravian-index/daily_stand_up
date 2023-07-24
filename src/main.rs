184c6f5f1e3cf279a35e13b7edb7dd93b5999064
David Chavez <depch47@gmail.com>
2023-07-24 09:36:23 -0500

    Starting project

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+// show participants
+// be able to pick participants out
+//
+
+struct Participant {
+    name: String,
+    has_spoken: bool,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn get_participants() {
+    let p : Vec<String> = vec![
+        String::from("David"),
+        String::from("Samuel"),
+        String::from("Nacho"),
+    ];
+}
+
+fn display_participants(participants: Participant) {
+    println!("------Today------")
+
+}
+
+// fn
