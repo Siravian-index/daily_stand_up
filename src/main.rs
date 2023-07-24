f531deba0db2eb7073494ddeb8d09dc85729738e
David Chavez <depch47@gmail.com>
2023-07-24 09:57:06 -0500

    Building structs and logic

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,51 @@
-// show participants
-// be able to pick participants out
-//
-
 struct Participant {
     name: String,
     has_spoken: bool,
+    id: i32,
 }
 
+impl Participant {
+    fn new(name: String, id: i32) -> Self {
+        Participant {
+            name,
+            has_spoken: false,
+            id,
+        }
+    }
+
+    fn format(&self) {
+        // display info
+        let f = format!("");
+    }
+
+    fn flip_spoken(id: i32) {
+
+    }
+}
+
+struct ParticipantManager {
+    participants: Vec<Participant>,
+}
+
+impl ParticipantManager {
+    fn new(vec: Vec<String>) -> Self {
+        let p = vec
+            .iter()
+            .enumerate()
+            .map(|(i, n)| Participant::new(n.clone(), i as i32))
+            .collect();
+        ParticipantManager { participants: p }
+    }
+
+    // display participants
+    fn display_current(&self) {
+        self.participants.iter().for_each(|p| p.format())
+    }
+
+    // mark participant that spoke
+
+    //
+}
 fn main() {
     println!("Hello, world!");
 }
-
-fn get_participants() {
-    let p : Vec<String> = vec![
-        String::from("David"),
-        String::from("Samuel"),
-        String::from("Nacho"),
-    ];
-}
-
-fn display_participants(participants: Participant) {
-    println!("------Today------")
-
-}
-
-// fn
